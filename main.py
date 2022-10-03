c3a471440dc5ae342d7d09c711f3cf5c48e2a4de
Abood K. AMA <abdamapro22@gmail.com>
2022-10-03 22:40:06 +0300

    finished :D

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 courses_list = []
 students_list = []
 students_IDs_list = []
+courses_names_list = []
 
 class Course:
     def __init__(self, course_name, course_class):
@@ -16,27 +17,21 @@ class Student:
         self.student_ID = student_ID
         self.student_name = student_name
         self.student_class = student_class
-        self.Student_courses_list = []
+        self.student_courses_list = []
 
     def add_course(self, course_name):
 
         for i in courses_list:
             if course_name == i.course_name:
                 if self.student_class == i.course_class:
-                    self.Student_courses_list.append(i.course_name)
+                    self.student_courses_list.append(i.course_name)
                 else:
                     print("class is not the same")
-            else:
 
-                print("course doesn't exist")
 
     def student_details(self):
-        print("-----------------")
-        print("Student ID \t\t Student name \t\t Student class \t\t Student courses")
 
-        print("  ",self.student_ID, "\t\t\t\t", self.student_name, "\t\t\t\t\t   ", self.student_class, "\t\t\t\t", self.Student_courses_list)
-
-
+        print("  ",self.student_ID, "\t\t\t\t", self.student_name, "\t\t\t\t   ", self.student_class, "\t\t\t\t", self.student_courses_list)
 while True:
     try:
         choice = int(input("----------------------------\n"
@@ -46,7 +41,6 @@ while True:
     except Exception:
         print("Please Enter number from the list :D")
         continue
-
     if choice == 1:
         student_name = input("Enter student name: ")
         while True:
@@ -61,13 +55,15 @@ while True:
         student = Student(student_ID, student_name, student_class)
         students_list.append(student)
         print("-----------------")
-        print("student ", student_name, "saved successfully")
+        print("student", student_name, "saved successfully")
         print("-----------------")
 
+        print("-----------------")
+        print("Student ID \t\t Student name \t\t Student class \t\t Student courses")
+
+
+
         student.student_details()
-        print(students_IDs_list)
-
-
     elif choice == 2:
         while True:
 
@@ -76,23 +72,20 @@ while True:
                 break
 
             try:
-                student_ID_check = int(input("Enter student ID ( enter 0 to exit): "))
+                student_ID_check_remove = int(input("Enter student ID ( enter 0 to exit): "))
             except Exception:
                 print("Please Enter ID number :D")
                 continue
 
-            if student_ID_check in students_IDs_list:
-                index_del = students_IDs_list.index(student_ID_check)
+            if student_ID_check_remove in students_IDs_list:
+                index_del = students_IDs_list.index(student_ID_check_remove)
                 del students_list[index_del]
-                students_IDs_list.remove(student_ID_check)
-                print("student with ID", student_ID_check, "is deleted successfully ")
+                students_IDs_list.remove(student_ID_check_remove)
+                print("student with ID", student_ID_check_remove, "is deleted successfully ")
                 break
-            elif student_ID_check ==0:
+            elif student_ID_check_remove ==0:
                 break
             print("ID doesn't exist")
-
-
-
     elif choice == 3:
         while True:
 
@@ -101,56 +94,84 @@ while True:
                 break
 
             try:
-                student_ID_check = int(input("Enter student ID ( enter 0 to exit): "))
+                student_ID_check_edit = int(input("Enter student ID ( enter 0 to exit): "))
             except Exception:
                 print("Please Enter ID number :D")
                 continue
 
-            if student_ID_check in students_IDs_list:
-                index_edit = students_IDs_list.index(student_ID_check)
+            if student_ID_check_edit in students_IDs_list:
+                index_edit = students_IDs_list.index(student_ID_check_edit)
 
                 students_list[index_edit].student_name = input("Enter new student name: ")
                 while True:
                     new_class = input("Enter student class from (A, B, C): ")
                     if new_class in ['A', 'B', 'C']:
                         students_list[index_edit].student_class = new_class
-                        print(students_list[index_edit].student_details())
                         break
-
-
-                print("Data of student with ID", student_ID_check, "is edited successfully ")
+                print("Data of student with ID", student_ID_check_edit, "is edited successfully ")
                 break
-            elif student_ID_check == 0:
+            elif student_ID_check_edit == 0:
                 break
             print("ID doesn't exist")
-
     elif choice == 4:
         while True:
             if len(students_IDs_list) == 0:
                 print("there are no students to search")
                 break
+            print("-----------------")
+            print("Student ID \t\t Student name \t\t Student class \t\t Student courses")
             for i in  students_list:
                 i.student_details()
             break
-
-
     elif choice == 5:
         course_name = input("Enter course name: ")
         while True:
             course_class = input("Enter course class from (A, B, C): ")
             if course_class in ['A', 'B', 'C']:
                 break
-        Course = Course(course_name, course_class)
-        courses_list.append(Course)
+        course = Course(course_name, course_class)
+        courses_list.append(course)
+        courses_names_list.append(course_name)
         print("Course", course_name, "saved successfully in class", course_class)
-
-
     elif choice == 6:
-        print(choice)
+        while True:
+
+            if len(students_IDs_list) == 0:
+                print("there are no students")
+                break
+            if len(courses_list) == 0:
+                print("there are no courses to add")
+                break
 
 
+            try:
+                student_ID_check_add = int(input("Enter student ID ( enter 0 to exit): "))
+            except Exception:
+                print("Please Enter ID number :D")
+                continue
+
+            if student_ID_check_add in students_IDs_list:
+                index_add_s = students_IDs_list.index(student_ID_check_add)
+                while True:
+                    course_add = input("Enter course name (enter 0 to exit): ")
+                    if course_add in courses_names_list:
+                        index_add_c = courses_names_list.index(course_add)
+                        if courses_list[index_add_c].course_class == students_list[index_add_s].student_class :
+                            students_list[index_add_s].student_courses_list.append(course_add)
+                            print("course",course_add,"is added successfully to student with ID",student_ID_check_add)
+                            break
+                        else:
+                            print("course class is different than student class")
+                    elif course_add == str(0):
+                        break
+                    else:
+                        print("course doesn't exist")
+                break
+            elif student_ID_check_add == 0:
+                break
+            else:
+                print("ID doesn't exist")
     elif choice == 0:
-        print(students_IDs_list)
         exit()
     else:
         print("Please Enter number from the list :D")
